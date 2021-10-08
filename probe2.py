58bd9c00940d5f43fec489590f7b1db76164c0eb
ripserj <all4tv@yandex.ru>
2021-10-08 15:28:22 +0300

    Разбирался с документированием функций

--- probe2.py
+++ probe2.py
@@ -1,11 +1,21 @@
-a = [int(x) for x in input().split(',')]
-y = int(input())
+def my_func():
+    """Do X and return a list."""
+    pass
+
+print(my_func.__doc__)
+
+
+jj = [4, 5, 7, 1, 2, 3, 12]
+y = 14
 flag1 = 0
 flag2 = 0
-for elem in a:
+for elem in jj:
     razn = elem - y
-    for i in a: 
+    for i in jj:
         if i == razn:
             flag1 = i
             flag2 = elem
-print(flag1, flag2) 
+print(flag1, flag2)
+
+
+
