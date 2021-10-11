48ea0f598f8806daa44b8b25f1db5b33846cb9e1
SERJ-NOUT <all4tv@yandex.ru>
2021-10-11 13:29:30 +0300

    Добавил обход дерева HTML_DOM

---
+++ рекурсия.py
@@ -0,0 +1,40 @@
+def factorial(num):
+    """ ПОИСК ФАКТОРИАЛА ЧИСЛА"""
+    if num == 1:
+        return 1
+    else:
+        return num*factorial(num-1)
+
+print(factorial(3))
+
+
+
+def find_element_in_html_code(tree, elem):
+    if elem in tree:
+        return tree[elem]
+    for key, sub_tree in tree.items():
+        if isinstance(sub_tree, dict):
+            res = find_element_in_html_code(sub_tree, elem)
+            if res:
+                break
+    else:
+        res = None
+    return res
+
+
+html_code = {
+    'html': {
+        'head': {
+            'title': "Заголовок страницы",
+            'link': "ссылка на CSS"
+        },
+        'body': {
+            'h1': "Главный заголовок",
+            'h2': "Подзаголовок",
+            'div': "Жили были кит и кот",
+            'p': "Кот и кит - наоборот"
+        }
+    }
+}
+
+print(find_element_in_html_code(html_code, "div"))
