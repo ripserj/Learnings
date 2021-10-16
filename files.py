76ed65718b84953a0980f22404293f3806289f1d
SERJ-NOUT <all4tv@yandex.ru>
2021-10-16 21:34:17 +0300

    Повторяю пространства имен

--- files.py
+++ files.py
@@ -1,3 +1,4 @@
 with open("repka3.txt", "r", encoding="UTF8") as f:
     for elem in f:
         print(elem, end="%%%%%%%%%%%%%\n")
+        print("Новая строка")
---
+++ namespaces.py
@@ -0,0 +1,12 @@
+def my_func():
+    global x
+    x = 'll'
+    print(f'эта переменная x внутри функции - {x}')
+
+
+my_func()
+print(x)
+
+
+
+
