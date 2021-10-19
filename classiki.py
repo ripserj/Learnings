b380c59a44cf6b7bbd6d40f92851f9c09fa88c93
SERJ-NOUT <all4tv@yandex.ru>
2021-10-19 22:47:13 +0300

    Классы разбираю __del__, __str__

--- classiki.py
+++ classiki.py
@@ -4,6 +4,12 @@ class MyCars:
         self.power = 124
         self.speed = 0
 
+    def __del__(self):
+        print("Перед тем как попасть на свалку была машина цвета: ", self.color)
+
+    def __str__(self):
+        return "Печатаем состояние объекта:" + self.color + ', ' + str(self.power) + ', ' + str(self.speed)
+
 
 first_car = MyCars()
 second_car = first_car
@@ -17,4 +23,5 @@ print(second_car is first_car)
 attrib = 'height2'
 print("Есть такой атрибут" if hasattr(first_car, attrib) else setattr(first_car, attrib, 55))
 
+print(first_car)
 print(getattr(first_car, attrib))
