e7a0b7d0c9eac67cf8adf8215bf429389bd6ed70
ripserj <all4tv@yandex.ru>
2021-10-20 14:48:45 +0300

    Переменные класса и переменные объекта

---
+++ atribyt_classa.py
@@ -0,0 +1,17 @@
+class MyResume:
+    """ если у объекта нет переменной, то он пользуется переменной класса"""
+    total_resume = 0
+    zagolovok = "Моё резюме"
+    def __init__(self):
+        MyResume.total_resume += 1
+    def __str__(self):
+        return self.zagolovok + ': ' + str(self.total_resume)
+
+resume_1 = MyResume()
+resume_2 = MyResume()
+resume_3 = MyResume()
+
+print(MyResume.total_resume)
+resume_1.zagolovok = "Нет резюме"
+print(resume_1)
+print(resume_2)
