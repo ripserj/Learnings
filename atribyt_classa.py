7e9c261e6e801bacfcaba05d97277859176e1d82
ripserj <all4tv@yandex.ru>
2021-10-20 14:58:41 +0300

    Переменные класса и переменные объекта, поправил по PEP8

--- atribyt_classa.py
+++ atribyt_classa.py
@@ -2,10 +2,13 @@ class MyResume:
     """ если у объекта нет переменной, то он пользуется переменной класса"""
     total_resume = 0
     zagolovok = "Моё резюме"
+
     def __init__(self):
         MyResume.total_resume += 1
+
     def __str__(self):
         return self.zagolovok + ': ' + str(self.total_resume)
+
 
 resume_1 = MyResume()
 resume_2 = MyResume()
