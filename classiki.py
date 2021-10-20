42ea4fa53718de2fbe60c7ef1ab6320028728a81
ripserj <all4tv@yandex.ru>
2021-10-20 12:38:37 +0300

    Описал класс как конструктор функций

--- classiki.py
+++ classiki.py
@@ -10,6 +10,32 @@ class MyCars:
     def __str__(self):
         return "Печатаем состояние объекта:" + self.color + ', ' + str(self.power) + ', ' + str(self.speed)
 
+    def __add__(self, other):
+        sum_car_power = []
+        if isinstance(other, MyCars):
+            sum_car_power.append(self.power)
+            sum_car_power.append(other.power)
+        elif isinstance(other, list):
+            sum_car_power.append(self.power)
+            sum_car_power.append(*other)
+
+        return sum_car_power
+
+    def __iadd__(self, other):
+        self.power = self.power + other.power
+        return self.power
+
+class MyFunctions:
+    def __init__(self, dobavka=10):
+        self.dobavka = dobavka
+
+    def __call__(self, *args, **kwargs):
+        for item in args:
+            print("Вывод функции-объекта класса:", item+self.dobavka)
+        for item in kwargs.values():
+            print("Вывод функции-объекта класса:", int(item)+self.dobavka)
+
+
 
 first_car = MyCars()
 second_car = first_car
@@ -25,3 +51,16 @@ print("Есть такой атрибут" if hasattr(first_car, attrib) else se
 
 print(first_car)
 print(getattr(first_car, attrib))
+
+third_car = MyCars()
+third_car.power = 320
+# print(first_car+[16])
+
+first_car += third_car
+print(first_car)
+
+
+func_1 = MyFunctions(dobavka=1000)
+func_1(a=3,b=8)
+func_1(a=1,b=5)
+func_1(12,18)
