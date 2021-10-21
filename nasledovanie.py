3d2fe68f4215a94549c1196456d7c1e418256fe5
ripserj <all4tv@yandex.ru>
2021-10-21 16:28:48 +0300

    Наследование - проверка доступа к методам и атрибутам родительского класс, инкапусляция данных

--- nasledovanie.py
+++ nasledovanie.py
@@ -48,3 +48,34 @@ print("+++++++++++++++++++++++ +++++++++++++++++++++++++")
 track1.tyres = 5
 track1.inspect()
 print(track1.tyres)
+
+class Parent():
+    class_var1 = 11
+    class_var2 = 21
+    class_var3 = 31
+
+    def __init__(self):
+        self.var_1 = 12
+        self._var_2 = 22
+        self.__var_3 = 32
+
+    def parent_method(self):
+        print(self.class_var1)
+        print(self.class_var2)
+        print(self.class_var3)
+        print(self.var_1)
+        print(self._var_2)
+       # print(self.__var_3)
+
+class Child(Parent):
+    def child_method(self):
+        print(self.class_var1)
+        print(self.class_var2)
+        print(self.class_var3)
+        print(self.var_1)
+        print(self._var_2)
+       # print(self.__var_3)
+
+obj = Child()
+obj.parent_method()
+obj.child_method()
